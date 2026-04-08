6eb5349a40c5b9179dd38106b6de491c37caf0f8
Ubuntu <ubuntu@ip-172-31-11-223.ap-south-1.compute.internal>
2026-04-08 12:17:40 +0000

    test ci/cd working

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Working Successfully 🚀 - Auto Deployed via Jenkins');
+  res.send('TEST SUCCESS 🚀');
 });
 
 app.listen(3000, () => {
